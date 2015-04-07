3a27cbd6e3771002f27ad7f755eea6f60f2cc049
John Biesnecker <jbiesnecker@gmail.com>
2015-04-07 10:45:18 -0500

    New post

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 
 jekyll build
 cd _site
-s3cmd sync --recursive . s3://biesnecker.com/ --delete-removed --cf-invalidate
+s3cmd sync --recursive . s3://biesnecker.com/ --delete-removed --cf-invalidate --cf-invalidate-default-index
 cd ..
